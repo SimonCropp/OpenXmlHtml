2cc85e04920d02b9ef458ebd12e1561749ee2e93
Simon Cropp <simon.cropp@gmail.com>
2026-04-13 13:19:46 +1000

    .

--- src/OpenXmlHtml.Tests/ModuleInitializer.cs
+++ src/OpenXmlHtml.Tests/ModuleInitializer.cs
@@ -5,9 +5,6 @@ public static class ModuleInitializer
     {
         VerifierSettings.DontScrubDateTimes();
         VerifierSettings.DontScrubGuids();
-#if NET10_0_OR_GREATER
-        VerifyImageSharp.Initialize(ssimThreshold: 0.99);
-#endif
         VerifierSettings.InitializePlugins();
         VerifierSettings.UniqueForRuntime();
         VerifyOpenXmlConverter.Initialize();
